84b07dd12851ce7ac189058f24730bc47476899f
Raahaf-6 <raahafyy@gmail.com>
2024-12-18 10:29:18 +0300

    Initial Commit

---
+++ scanTestt6/scanTestt6App.swift
@@ -0,0 +1,17 @@
+//
+//  scanTestt6App.swift
+//  scanTestt6
+//
+//  Created by Rahaf on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct scanTestt6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scanTestt6Tests/scanTestt6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  scanTestt6Tests.swift
+//  scanTestt6Tests
+//
+//  Created by Rahaf on 18/12/2024.
+//
+
+import Testing
+@testable import scanTestt6
+
+struct scanTestt6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
